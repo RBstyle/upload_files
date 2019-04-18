be7a35d927e8b909b4f37bf2c132dde9850b38c1
Ruslan Yusupov <rbyusupov@gmail.com>
2019-04-18 18:49:59 +0300

    first edition + SQLAlchemy

--- content.py
+++ content.py
@@ -1,30 +1,8 @@
-import sqlite3
 import os
+
 
 def get_list_uploads():
     uploads_count = []
     for filename in os.listdir('uploads/'):
         uploads_count.append(filename)
     return uploads_count
-
-'''
-следующие функции не актуальны.
-'''
-def connect_db(query=None):
-    if query == 'commit':
-        con.commit()
-        print('Commit exept')
-        return None
-    con = sqlite3.connect('./data/info.db')
-    cur = con.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS incoming_files(Filename TEXT, Url TEXT, IP adress text)')
-    return cur
-
-def insert_value(filename, ip):
-    print('start')
-    cur = connect_db()
-    data = [filename, 'uploads/' + filename, ip]
-    cur.execute('INSERT INTO incoming_files VALUES(?, ?, ?)', data)
-    connect_db('commit')
-    print('end')
-    return None
